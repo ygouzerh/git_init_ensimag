c765b3f43655699b5e065d0644df02d6ae88762d
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.og>
2017-11-16 15:53:05 +0100

    Adding of the main script and documentation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # git_init_ensimag
+
+## Description
 Automate tool for creating git repositories on depots.ensimag.fr
+
+This script automate the creation of a git on depots.ensimag.fr
+Allow you to share a git with other colleagues.
+> Add multiple colleague could be done by : '-c pseudo1 -c pseudo1' .....
+
+## Usage
+$(basename "$0")  --gitname name_of_the_git [options]
+>Example : ./git_init.sh -g tp.git -c dupontj -c dupondj -f our_folder -v
+
+## Required arguments
+--gitname | -g       Name of the .git
+
+## Options
+--help | -h          Display help message
+--foldername | -f    Name of the folder on the server
+--colleague | -c     List of the pseudo of your colleague
+
+## Contributions
+
+Feel free to contribute ! All improvements and helps are welcomed.
---
+++ git_init.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# title : git_init.sh
+# author : ygouzerh
+# description : Automating tool for creating git repositories on depots.ensimag.fr
+# date : 16 November 2017
+# version : 1.0
+
+# ---------- CONSTANTS ----------
+
+# List of the colleagues. Could be empty
+COLLEAGUE=""
+# Name of the .git
+GITNAME=""
+# Name of the folder
+FOLDER_NAME=""
+# Print what the script do if verbose is on
+VERBOSE=false
+# Current year. Ex : 2017
+YEAR=$(date +"%Y")
+
+# ---------- FUNCTIONS ----------
+
+# Usage : DISPLAY THE HELP
+usage () {
+    cat << EOF
+HELP -- '$(basename "$0")' :
+
+This script automate the creation of a git on depots.ensimag.fr
+Allow you to share a git with other colleagues.
+>> Add multiple colleague could be done by : '-c pseudo1 -c pseudo1' .....
+
+Usage: $(basename "$0")  --gitname name_of_the_git [options]
+Example : ./git_init.sh -g tp.git -c dupontj -c dupondj -f our_folder -v
+
+Required arguments :
+         --gitname | -g       Name of the .git
+Options: --help | -h          Display help message
+         --foldername | -f    Name of the folder on the server
+         --colleague | -c     List of the pseudo of your colleague
+EOF
+}
+
+# Verbose print is first argument if verbose is on
+verbose() {
+    if [ "$VERBOSE" = true ]; then
+        echo "$(basename "$0") > $1"
+    fi
+}
+
+# ---------- RECOVERY OF ARGUMENTS ----------
+while test $# -ne 0; do
+    case "$1" in
+        "--help"|"-h") # Display help and quit
+            usage
+            exit 0
+            ;;
+        "--gitname" | "-g") # Name of the git
+            shift;
+            if [ ! "$1" = "" ]; then
+                GITNAME="$1"
+            else
+                echo "You need to pass the name of your git. Ex : --gitname | -g tp.git"
+                exit
+            fi
+            ;;
+        "--colleague" | "-c") # List of the colleagues
+            shift;
+            # Test if have colleagues or not
+            if [ ! "$1" = "" ]; then
+                if [ "$COLLEAGUE" = "" ]; then
+                    COLLEAGUE="$1"
+                else
+                    COLLEAGUE="$COLLEAGUE"" ""$1"
+                fi
+            else
+                echo "You need to pass the pseudo of your colleague. Ex : --colleague | -c dupontj martinl"
+                exit
+            fi
+            ;;
+        "--foldername" | "-f") # Name of the folder
+            shift;
+            # Test if have colleagues or not
+            if [ ! "$1" = "" ]; then
+                FOLDER_NAME="$1"
+            else
+                echo "You need to pass the name of the folder. Ex : --foldername | -f folder_of_my_git"
+                exit
+            fi
+            ;;
+        "--verbose" | "-v") # Détaille l'éxecution du script
+            VERBOSE=true
+        ;;
+        *) # if an argument is not recognized, it will show the help and kill the program
+            echo "Argument is not recognized : $1"
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# The name of the git is mandatory
+if [ "$GITNAME" = "" ]; then
+    echo "You need to pass the name of your git. Ex : --gitname | -g tp.git"
+    exit;
+fi
+
+# Possibility to give a folder name by default
+if [ "$FOLDER_NAME" = "" ]; then
+    # Folder name (default): on the form : 'user_pseudo1_pseudo2'. Could be only 'user'
+    FOLDER_NAME=$USER
+    if [ ! "$COLLEAGUE" = "" ]; then
+        FOLDER_NAME=$FOLDER_NAME"_""${COLLEAGUE// /_}"
+    fi
+fi
+
+verbose "Constants : "
+verbose "   YEAR : $YEAR";
+verbose "   COLLEAGUE : $COLLEAGUE";
+verbose "   GITNAME : $GITNAME";
+verbose "   FOLDER_NAME : $FOLDER_NAME"
+
+# ---------- CREATION ----------
+
+# LOGIN TO THE SERVER
+
+# EOF allow to exit the ssh connection
+ssh "$USER@depots.ensimag.fr" << EOF
+
+    # CREATION OF THE FOLDER
+    cd "/depots/$YEAR" || exit
+    mkdir "$FOLDER_NAME"
+    echo "$(basename "$0") > Create the folder"
+
+    cd "$FOLDER_NAME" || exit
+    echo "$(basename "$0") > Go into the folder"
+
+    # GIVE THE RIGHTS
+    chmod go-rwx .
+    echo "$(basename "$0") > Restrinct to others the access to the folder"
+    autoriser-equipe . $COLLEAGUE
+
+
+    # CREATION OF THE GIT
+    git init --shared --bare "$GITNAME"
+    echo "$(basename "$0") > Git created."
+
+    exit
+
+EOF
+
+URL="ssh://$USER@depots.ensimag.fr/depots/$YEAR/$FOLDER_NAME/$GITNAME"
+verbose "Exit the server"
+verbose "End creation of the git."
+echo "URL : $URL"
